11aa173e649d798a5167d4fc9ef935751eac8f0c
M16Yusuf <yusufsmd58@gmail.com>
2019-10-27 20:45:13 +0700

    add anms

---
+++ js/myscript.js
@@ -0,0 +1,13 @@
+// Loading Page
+
+var myVar;
+function myFunction() {
+        myVar = setTimeout(showPage, 500);
+    }
+
+
+
+function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("myDiv").style.display = "block";
+}
